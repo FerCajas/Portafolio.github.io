73bfc8b674f253a748eaabd96647c23dd9b360dd
FerCajas <87354391+FerCajas@users.noreply.github.com>
2024-04-26 20:34:05 -0500

    Update mai2.js
    Borrar comentario mai2.js

--- JavaScript/mai2.js
+++ JavaScript/mai2.js
@@ -1,19 +1,19 @@
 function openModal(videoSrc) {
     var modal = document.getElementById('videoModal');
     var video = document.getElementById('modalVideo');
-    video.src = videoSrc; // Establecer la fuente del video
-    modal.style.display = 'block'; // Mostrar el modal
+    video.src = videoSrc; 
+    modal.style.display = 'block'; 
 }
 
 // Función para cerrar el modal
 function closeModal() {
     var modal = document.getElementById('videoModal');
     var video = document.getElementById('modalVideo');
-    video.pause(); // Pausar el video al cerrar el modal
-    modal.style.display = 'none'; // Ocultar el modal
+    video.pause(); 
+    modal.style.display = 'none'; 
 }
 
-// Cerrar el modal si el usuario hace clic fuera del área del modal
+
 window.onclick = function(event) {
     var modal = document.getElementById('videoModal');
     if (event.target == modal) {
