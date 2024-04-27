05556288943cc044ce2503075a700b952f76a110
FerCajas <87354391+FerCajas@users.noreply.github.com>
2024-04-26 20:32:55 -0500

    Update main.js
    Borrar comentarios en main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,6 +1,6 @@
 
 
-// Definir autobiografía por años
+
 var autobiography = [
     { year: 2020, event: "Fundamentos de la web", description: "Tome cursos en linea para aprender HTML,CSS y JS.", image: "../Images/Udemy.jpg" },
     { year: 2020, event: "Desarrollo web", description: "Durante la virtualidad he conocido variedad de tecnologias para el desarrollo web.", image: "../Images/Tecnologiaweb.jpg" },
@@ -9,28 +9,28 @@ var autobiography = [
     { year: 2023, event: "Proyecto propio", description: "Desarrollo web con angular.", image: "../Images/WH.png" }
 ];
 
-var currentYearIndex = 0; // Índice del año actual en la autobiografía
+var currentYearIndex = 0; 
 
-// Función para abrir la ventana modal al hacer clic en "Comenzar"
+
 function openModal() {
     var modal = document.getElementById("modal");
     var modalText = document.getElementById("modal-text");
     var modalImage = document.getElementById("modal-image");
 
-    // Mostrar el primer año de la autobiografía en la modal
+    
     displayYearInfo(currentYearIndex);
 
-    // Abrir la ventana modal
+    
     modal.style.display = "block";
 }
 
-// Función para cerrar la ventana modal al hacer clic en la "X"
+
 function closeModal() {
     var modal = document.getElementById("modal");
-    modal.style.display = "none"; // Ocultar la ventana modal
+    modal.style.display = "none";
 }
 
-// Función para mostrar la información del año específico en la autobiografía
+
 function displayYearInfo(index) {
     var modalText = document.getElementById("modal-text");
     var modalImage = document.getElementById("modal-image");
@@ -42,14 +42,14 @@ function displayYearInfo(index) {
     }
 }
 
-// Función para mostrar la información del año siguiente en la autobiografía
+
 function showNextYear() {
-    currentYearIndex++; // Incrementar el índice al siguiente año
+    currentYearIndex++; 
     displayYearInfo(currentYearIndex);
 }
 
-// Función para mostrar la información del año anterior en la autobiografía
+
 function showPreviousYear() {
-    currentYearIndex--; // Decrementar el índice al año anterior
+    currentYearIndex--; 
     displayYearInfo(currentYearIndex);
 }
